d2db8c2f970e40c8b144c9f8f7da5dff46e85356
Auporna-Barua <aupornabarua@gmail.com>
2023-05-26 11:48:33 -0700

    sigle recipes get api make

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const chefs = require("./data/chefData.json");
+const recipes = require("./data/recipes.json");
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("Healthy food is running");
 });
+
 // main code Start
+
 app.get("/chefs", (req, res) => {
   res.send(chefs);
 });
+
 app.get("/recipes", (req, res) => {
   res.send(recipes);
+});
+
+app.get("/recipes/:id", (req, res) => {
+  const id = req.params.id;
+  const recipe = recipes.filter((rec) => rec.id == id);
+  res.send(recipe);
 });
 
 
