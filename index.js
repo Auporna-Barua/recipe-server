3398dc51f08c22457645e12becbdd068b992fd5c
Auporna-Barua <aupornabarua@gmail.com>
2023-05-26 11:45:21 -0700

    all chefs get api make

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.get("/", (req, res) => {
-  res.send("food city is running");
+  res.send("Healthy food is running");
 });
 // main code Start
-
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
 
 
 
 
 // main code End
 app.listen(port, () => {
-  console.log(`food city server is running from ${port}`);
+  console.log(`Healthy food server is running from ${port}`);
 });
